4e72af643c75304da543b84c7dac29e13d633bc9
Mathieu Battah <matbattah@hotmail.com>
2012-11-16 13:27:18 -0500

    Initial commit avec un interface.

---
+++ src/clientforme/ApplicationSupport.java
@@ -0,0 +1,109 @@
+package clientforme;
+/*
+ * Ce code provient d'un exemple sur Internet, de David Geary, à la page suivante:
+ * http://www.javaworld.com/javaworld/jw-05-2003/jw-0530-designpatterns-p2.html
+ * 
+ *  A Swing application façade
+ *  Swing is one of my all-time favorite GUI frameworks, but even after writing 
+ * a 1,600-page book on the subject, I have to refer to the book and Javadocs 
+ * almost every time I write a Swing application. Just getting a Swing application 
+ * off the ground can require heavy lifting, especially if you follow best 
+ * practices by internationalizing your application. This section shows you how to 
+ * implement a façade that encapsulates much of the grunt work involved in setting 
+ * up a Swing application.
+ */
+import java.awt.*;
+import java.awt.event.*;
+import java.text.MessageFormat;
+import java.util.*;
+import javax.swing.*;
+
+public final class ApplicationSupport {
+   static private final String PREFS = "prefs";
+   static private final String BUNDLE_BASENAME = "app", 
+   							   LOCALE_KEY = "locale";
+
+   static private final JPanel STATUS_AREA = new JPanel();
+   static private final JLabel STATUS = new JLabel();
+   static private ResourceBundle preferences, resources;
+   static private Locale locale;
+
+   static {
+      try {
+         preferences = ResourceBundle.getBundle(PREFS);
+         locale = new Locale(preferences.getString(LOCALE_KEY));
+      }
+      catch(java.util.MissingResourceException ex) {
+         System.err.println("ERROR: cannot find preferences properties file " + 
+                            BUNDLE_BASENAME);
+      }
+
+      try {
+         resources = ResourceBundle.getBundle(BUNDLE_BASENAME, locale);
+      }
+      catch(java.util.MissingResourceException ex) {
+         System.err.println("ERROR: cannot find properties file for " + BUNDLE_BASENAME);
+      }
+
+   };
+
+   // disallow direct instantiation
+   private ApplicationSupport() {}
+   
+   public static void launch(final JFrame jFrame, String title,
+                       final int xBounds, final int yBounds, 
+                       final int width, int height) {
+      jFrame.setTitle(title);
+      jFrame.setBounds(xBounds,yBounds,width,height);
+      jFrame.setVisible(true);
+      jFrame.setResizable(true);
+
+      STATUS.setHorizontalAlignment(JLabel.LEFT);
+
+      STATUS_AREA.setBorder(BorderFactory.createEtchedBorder());
+      STATUS_AREA.setLayout(new FlowLayout(FlowLayout.LEFT,0,0));
+      STATUS_AREA.add(STATUS);
+
+      jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+      jFrame.addWindowListener(new WindowAdapter() {
+         public void windowClosed(WindowEvent event) {
+            System.exit(0);
+         }
+      });
+   }
+   public static Locale getLocale() {
+      return locale;
+   }
+   public static JMenu addMenu(final JFrame jFrame, String titleKey,
+                               String[] itemKeys) {
+
+      JMenuBar menuBar = jFrame.getJMenuBar();
+      if(menuBar == null) {
+         menuBar = new JMenuBar();
+         jFrame.setJMenuBar(menuBar);
+      }
+
+      JMenu menu = new JMenu(ApplicationSupport.getResource(titleKey));
+
+      for(int i=0; i < itemKeys.length; ++i) {
+         menu.add(new JMenuItem(ApplicationSupport.getResource(itemKeys[i])));
+      }
+      menuBar.add(menu);
+      return menu;
+   }
+   public static JPanel getStatusArea() {
+      return STATUS_AREA;
+   }
+   public static void showStatus(String statusString) {
+      STATUS.setText(statusString);
+   }
+   public static String getResource(String key) {
+      return (resources == null) ? null : resources.getString(key);
+   }
+   public static String formatMessage(String patternKey, String[] params) {
+      String pattern = ApplicationSupport.getResource(patternKey);
+      MessageFormat fmt = new MessageFormat(pattern);
+      return fmt.format(params);
+   }
+}
---
+++ src/clientforme/ApplicationSwing.java
@@ -0,0 +1,342 @@
+package clientforme;
+/******************************************************
+ Cours :             LOG-121
+ Session :           Automne 2012
+ Groupe :            4
+ Projet :            Laboratoire #2
+Étudiant(e)(s) : 	Philippe Charbonneau
+				 	Patrice Robitaille
+				 	Mathieu Battah
+ Code(s) perm. :    CHAP07110906
+                    ROBP2002805 
+                    BATM19038902 
+ Professeur :        Groucho Marx
+ Date créée :        2002-05-28
+ Date dern. modif. : 2012-10-11
+ 
+*******************************************************
+ Historique des modifications
+*******************************************************
+  2002-05-28	Cris Fuhrman : Version initiale
+  
+  2004-03-07	Cris Fuhrman : Intégration de SwingWorker 
+                requierant la classe additionnelle 
+                SwingWorker.java, utilisation des variables 
+                constantes, formatage de code source, 
+                organisation des imports, etc.
+
+  2005-05-01	Cris Fuhrman : Intégration de ApplicationSupport
+  				requierant la classe additionnelle
+  				ApplicationSupport.java et les fichiers
+  				prefs.properties, app_xx.properties (où xx est le
+  				code de la langue, p. ex. fr = français, en = anglais).
+  				Suppression de l'interface Shape.
+  				
+  2006-05-03	Sébastien Adam :
+  
+                Uniformisation et maintenance du code.
+
+                Ajout des classes pour la gestion des
+                items de menu. Un écouteur ajouté pour chaque item 
+                (DemarrerListener, ArreterListener, QuitterListener, 
+                AProposDeListener).  
+                
+                La classe ApplicationSwing n'implémente plus ActionListener. 
+                Elle délègue la gestion des items.
+                
+                Plus besion d'un "if else if" dans la methode actionPerformed pour 
+                exécuter l'action associée à un item. Le code est plus
+                simple à comprendre, lire et maintenir.	
+                
+  2012-09-21    Patrice Robitaille:
+  
+  				Ajout d'une fenêtre dialog qui gère le input de connexion
+  				pour le serveur. Le script fait également une validation
+  				à la source et récupère le nom du serveur ainsi que le numéro
+  				de port à l'aide du délimiteur ':'.
+  				
+  				Ajout de 2 variables contenant le nom du serveur et le numéro de port
+  				à valider.
+  				
+  2012-09-28	Mathieu Battah
+  
+  				Ajout de l'utilisation de GestionForme pour gérer la liste de formes.
+  				
+  2012-10-11	Mathieu Battah
+  
+  				Modification des menus pour le lab 2.
+              
+
+ La distribution originale se trouve à 
+ https://cours.ele.etsmtl.ca/academique/log120/notesdecours/exemples/lab/lab1/ApplicationSwing.zip
+********************************************************/
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+import javax.swing.ButtonGroup;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
+import javax.swing.JScrollPane;
+import javax.swing.KeyStroke;
+
+/**
+ * <code>ApplicationSwing</code> est un exemple d'une
+ * application en Java qui fournit une interface Swing, avec un simple
+ * menu et un dessin.
+ *
+ * <h4>References</h4> 
+ * <ul> 
+ *
+ * <li>C. Fuhrman, &quot;Notes de cours de LOG120,&quot; &Eacute;cole
+ * de technologie sup&eacute;rieure, Montr&eacute;al, Qu&eacute;bec,
+ * Canada, 2002
+ *
+ * <li>Xemacs (for generation of the initial template), <a target="_top" 
+ * href="http://www.xemacs.org">www.xemacs.org</a>, 2002 
+ *
+ * <li><a target="_top" 
+ * href="http://java.sun.com/docs/books/tutorial/uiswing/painting/overview.html">Overview
+ * of Custom Painting</a>, une partie du tutoriel Java de Sun, 2002.
+ *
+ * <li>Java Software, <a target="_top" 
+ * href="http://java.sun.com/j2se/javadoc/writingdoccomments/index.html">&quot;How
+ * to Write Doc Comments for the Javadoc<sup>TM</sup> Tool,&quot;</a>
+ * 2002
+ *
+ * </ul>
+ *
+ * Distribution originale &agrave; partir du 
+ * <a target="_top" href="https://cours.ele.etsmtl.ca/academique/log120/">site Web</a>
+ * du cours LOG120.
+ * 
+ * Created: Tue May 28 11:31:18 2002
+ *
+ * @author <a href="mailto:christopher.fuhrman@etsmtl.ca">Christopher Fuhrman</a>
+ *
+ * @version 1.1
+ */
+
+public class ApplicationSwing extends JFrame {
+
+	private static final int CANEVAS_HAUTEUR = 500;
+
+	private static final int CANEVAS_LARGEUR = 500;
+
+	private static final int MARGE_H = 50;
+
+	private static final int MARGE_V = 60;
+	
+	private static final char FICHIER_RACC = KeyEvent.VK_F;
+	private static final int FORME_MASK = ActionEvent.CTRL_MASK;
+	private static final char FORME_RACC = KeyEvent.VK_O;
+	private static final int ORDRE_MASK = ActionEvent.CTRL_MASK;
+	private static final char ORDRE_RACC = KeyEvent.VK_R;
+	private static final char NOSEQASC_RACC = KeyEvent.VK_N;
+	private static final char NOSEQDESC_RACC = KeyEvent.VK_S;
+	private static final int QUITTER_MASK = ActionEvent.CTRL_MASK;
+	private static final char QUITTER_RACC = KeyEvent.VK_Q;
+	private static final char AIDE_RACC = KeyEvent.VK_A;
+	private static final int PROPOS_MASK = ActionEvent.CTRL_MASK;
+	private static final char PROPOS_RACC = KeyEvent.VK_P;
+
+	private static final String
+			FICHIER_TITRE = "app.frame.menus.file.title",
+			FICHIER_FORME = "app.frame.menus.file.getshape",
+			FICHIER_QUITTER = "app.frame.menus.file.exit",
+			ORDRE_TITRE = "app.frame.menus.order.title",
+			ORDRE_NOSEQASC = "app.frame.menus.order.nosequenceascending",
+			ORDRE_NOSEQDESC = "app.frame.menus.order.nosequencedescending",
+			AIDE_TITRE = "app.frame.menus.help.title",
+			AIDE_PROPOS = "app.frame.menus.help.about";
+
+	private static final String DIALOGUE_A_PROPOS = "app.frame.dialog.about";
+
+	private static final long serialVersionUID = 1L;
+	
+	private ButtonGroup groupeOrdre;
+	
+	private enum Ordre {NOSEQASC, NOSEQDESC};
+	
+	/**
+	 * Traiter les items du menu "Ordre".
+	 */
+	class OrdreAction extends AbstractAction{
+		private static final long serialVersionUID = 1L;
+		
+		public OrdreAction(String menuItemTitle, Ordre ordreTri){
+			super(menuItemTitle);
+		}
+		
+		public void actionPerformed(ActionEvent arg0){
+			//On rafraichit l'écran
+			repaint();
+			validate();
+		}
+	}
+	
+	/**
+	 *  Traiter l'item "Obtenir formes".
+	 */
+	class ObtenirFormesAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		
+		public ObtenirFormesAction() {
+			super(ApplicationSupport.getResource(FICHIER_FORME));
+		}
+		
+		public void actionPerformed(ActionEvent arg0) {
+		}
+	}
+	
+	/**
+	 *  Traiter l'item "Quitter".
+	 */
+	class QuitterAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+		
+		public QuitterAction() {
+			super(ApplicationSupport.getResource(FICHIER_QUITTER));
+		}
+		
+		public void actionPerformed(ActionEvent arg0) {
+			System.exit(0);
+		}
+	}
+	
+	/** 
+	 * Traiter l'item "A propos". 
+	 */
+	class AProposDeAction extends AbstractAction {
+		private static final long serialVersionUID = 1L;
+
+		public AProposDeAction(){
+			super(ApplicationSupport.getResource(AIDE_PROPOS));
+		}
+		
+		public void actionPerformed(ActionEvent arg0) {
+			JOptionPane.showMessageDialog(null, ApplicationSupport.getResource(DIALOGUE_A_PROPOS), ApplicationSupport.getResource(AIDE_PROPOS),JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+
+	/**
+	 *  Créer le panneau sur lequel les formes sont dessinées. 
+	 */
+	class CustomCanvas extends JPanel {
+		private static final long serialVersionUID = 1L;
+
+		public CustomCanvas() {
+			setSize(getPreferredSize());
+			setMinimumSize(getPreferredSize());
+			CustomCanvas.this.setBackground(Color.white);
+		}
+
+		public Dimension getPreferredSize() {
+			return new Dimension(CANEVAS_LARGEUR, CANEVAS_HAUTEUR);
+		}
+
+		public void paintComponent(Graphics graphics) {
+			super.paintComponent(graphics);
+			Graphics2D g2d = (Graphics2D) graphics;
+			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
+					RenderingHints.VALUE_ANTIALIAS_ON);
+
+			
+		}
+	}
+	
+	/* - Constructeur - Créer le cadre dans lequel les formes sont dessinées. */
+	public ApplicationSwing() {
+		getContentPane().add(new JScrollPane(new CustomCanvas()));
+	}
+
+	/* Créer le menu "Ordre". */
+	private JMenu creerMenuOrdre() {
+		JMenu menu = new JMenu(ApplicationSupport.getResource(ORDRE_TITRE));
+		menu.setMnemonic(ORDRE_RACC);
+		
+		groupeOrdre = new ButtonGroup();
+		
+		/* Création de JRadtioButtonMenuItem. */
+		JRadioButtonMenuItem ordreNoSeqCst = new JRadioButtonMenuItem(new OrdreAction(ApplicationSupport.getResource(ORDRE_NOSEQASC), Ordre.NOSEQASC));
+		JRadioButtonMenuItem ordreNoSeqDst = new JRadioButtonMenuItem(new OrdreAction(ApplicationSupport.getResource(ORDRE_NOSEQDESC), Ordre.NOSEQDESC));
+		
+		
+		/* Ajout des raccourcis spécifiques à chaque bouton radio. */
+		ordreNoSeqCst.setAccelerator(KeyStroke.getKeyStroke(NOSEQASC_RACC, ORDRE_MASK));
+		ordreNoSeqCst.setMnemonic(NOSEQASC_RACC);
+		ordreNoSeqDst.setAccelerator(KeyStroke.getKeyStroke(NOSEQDESC_RACC, ORDRE_MASK));
+		ordreNoSeqDst.setMnemonic(NOSEQDESC_RACC);
+
+		/* Ajout des boutons radio au groupe de radio bouton. */
+		groupeOrdre.add(ordreNoSeqCst);
+		groupeOrdre.add(ordreNoSeqDst);
+		
+		/* Ajout des boutons radio au menu. */
+		menu.add(ordreNoSeqCst);
+		menu.add(ordreNoSeqDst);
+
+		return menu;
+	}
+
+	/* Créer le menu "Fichier". */
+	private JMenu creerMenuFichier() {
+		JMenu menu = new JMenu(ApplicationSupport.getResource(FICHIER_TITRE));
+		menu.setMnemonic(FICHIER_RACC);
+		
+		menu.add(new ObtenirFormesAction());
+		menu.getItem(0).setAccelerator(KeyStroke.getKeyStroke(FORME_RACC, FORME_MASK));
+		menu.getItem(0).setMnemonic(FORME_RACC);
+		
+		menu.add(new QuitterAction());
+		menu.getItem(1).setAccelerator(KeyStroke.getKeyStroke(QUITTER_RACC, QUITTER_MASK));
+		menu.getItem(1).setMnemonic(QUITTER_RACC);
+
+		return menu;
+	}
+
+	/* Créer le menu "Aide". */
+	private JMenu creerMenuAide() {
+		JMenu menu = new JMenu(ApplicationSupport.getResource(AIDE_TITRE));
+		menu.setMnemonic(AIDE_RACC);
+
+		menu.add(new AProposDeAction());
+		menu.getItem(0).setAccelerator(KeyStroke.getKeyStroke(PROPOS_RACC, PROPOS_MASK));
+		menu.getItem(0).setMnemonic(PROPOS_RACC);
+
+		return menu;
+	}	
+	
+	/* Lancer l'exécution de l'application. */
+	public static void main(String[] args) {
+		
+		/* Créer la fenêtre de l'application. */
+		ApplicationSwing cadre = new ApplicationSwing();
+		
+		JMenuBar barreMenu = new JMenuBar();
+		barreMenu.add(cadre.creerMenuFichier());
+		barreMenu.add(cadre.creerMenuOrdre());
+		barreMenu.add(cadre.creerMenuAide());
+		cadre.setJMenuBar(barreMenu);
+
+		/* On récupère la dimension de l'écran pour centrer. */	
+		Point centre = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
+		
+		/* Lancer l'application. */
+		ApplicationSupport.launch(cadre, ApplicationSupport
+				.getResource("app.frame.title"), (centre.x - (CANEVAS_LARGEUR / 2)), (centre.y - (CANEVAS_HAUTEUR / 2)), CANEVAS_LARGEUR
+				+ MARGE_H, CANEVAS_HAUTEUR + MARGE_V);
+	}
+}
